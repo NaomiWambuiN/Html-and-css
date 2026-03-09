57bc30c84f6137696b1dcd22ecc19171c7bc6135
ADMIN <mburujoseph534@gmail.com>
2026-03-09 08:09:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+window.alert("Welcome to my portfolio website")
+    const btn = document.getElementById('mode-button');
+    
+    btn.addEventListener('click', function() {
+      // This "toggles" the class on and off each time you click
+      document.body.classList.toggle('dark-theme');
+    });
+
+  
